576c1d07a6b98af821bfe8bca6be622ccc8986fd
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2025-11-25 01:26:07 +0000

    derived required data for CIFAR100/100

--- CIFAR_read.py
+++ CIFAR_read.py
@@ -9,3 +9,25 @@ import matplotlib.pyplot as plt
 
 (x_train_100, y_train_100), (x_test_100, y_test_100) = cifar100.load_data(label_mode='fine')
 #CIFAR 100 
+
+cifar10_classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 
+                   'dog', 'frog', 'horse', 'ship', 'truck']
+cifar10_relevant = [1, 2, 3, 4, 5, 7, 9]  
+
+# Required data automobile, bird, cat, deer, dog, horse, truck
+
+
+cifar100_classes = [
+    'apple','aquarium_fish','baby','bear','beaver','bed','bee','beetle','bicycle','bottle',
+    'bowl','boy','bridge','bus','butterfly','camel','can','castle','caterpillar','cattle',
+    'chair','chimpanzee','clock','cloud','cockroach','couch','crab','crocodile','cup','dinosaur',
+    'dolphin','elephant','flatfish','forest','fox','girl','hamster','house','kangaroo','keyboard',
+    'lamp','lawn-mower','leopard','lion','lizard','lobster','man','maple_tree','motorcycle','mountain',
+    'mouse','mushroom','oak_tree','orange','orchid','otter','palm_tree','pear','pickup_truck','pine_tree',
+    'plain','plate','poppy','porcupine','possum','rabbit','raccoon','ray','road','rocket',
+    'rose','sea','seal','shark','shrew','skunk','skyscraper','snail','snake','spider',
+    'squirrel','streetcar','sunflower','sweet_pepper','table','tank','telephone','television','tiger','tractor',
+    'train','trout','tulip','turtle','wardrobe','whale','willow_tree','wolf','woman','worm'
+]
+
+cifar100_relevant = [19, 34, 2, 11, 35, 76, 38, 57, 84, 27, 8, 13, 48, 58, 92, 61, 99]  
