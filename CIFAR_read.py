0ade2fec8d7662e696812d50a8c54c00bb746e07
dianedalyop <114318000+dianedalyop@users.noreply.github.com>
2025-11-25 00:49:55 +0000

    Loading Data sets CIFAR10/100

--- CIFAR_read.py
+++ CIFAR_read.py
@@ -4,3 +4,8 @@ from keras import layers
 import numpy as np
 import matplotlib.pyplot as plt
 
+(x_train_10, y_train_10), (x_test_10, y_test_10) = cifar10.load_data()
+#CIFAR 10 
+
+(x_train_100, y_train_100), (x_test_100, y_test_100) = cifar100.load_data(label_mode='fine')
+#CIFAR 100 
